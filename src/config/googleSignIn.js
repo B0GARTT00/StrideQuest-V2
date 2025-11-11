70b585584ff8e1e663035782990a5c8b443a1bad
Kyle Lamigo <lamigokyle18@gmail.com>
2025-11-11 21:31:56 +0800

    Update Android Client ID for new keystore

--- src/config/googleSignIn.js
+++ src/config/googleSignIn.js
@@ -9,9 +9,9 @@ export const GOOGLE_EXPO_CLIENT_ID = '300404105909-kukr3gedqthqlnihgeaoaamcmc51m
 // 1. Go to https://console.cloud.google.com/apis/credentials
 // 2. Create OAuth 2.0 Client ID → Android
 // 3. Package name: com.stridequest.app
-// 4. SHA-1: 36:4C:D9:38:BC:73:D5:42:9B:0B:2C:18:79:CF:9B:0C:F3:43:CB:E7
+// 4. SHA-1: 46:F9:25:82:B9:F4:61:A4:75:04:BB:48:9D:C5:14:22:0E:31:07:D3
 // 5. Paste the generated Client ID below:
-export const GOOGLE_ANDROID_CLIENT_ID = '300404105909-ufciao6bbedc0mkcv0m4fcqrbvev26tv.apps.googleusercontent.com';
+export const GOOGLE_ANDROID_CLIENT_ID = '300404105909-nmgb8d3r6u5iblp435n8tscqm61f46sm.apps.googleusercontent.com';
 
 // iOS Client ID (if you have one)
 export const GOOGLE_IOS_CLIENT_ID = '300404105909-kukr3gedqthqlnihgeaoaamcmc51mkel.apps.googleusercontent.com';
